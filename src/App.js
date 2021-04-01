2b8f2dce769274bc09e7f8beac8c068b55645377
Nokibul Amin Mezba <csmezba@gmail.com>
2021-04-01 18:11:30 +0600

    React-Redux: Create Routes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,35 @@
 import React, { Component } from 'react';
 import Books from './components/Books';
 import CreateBook from './containers/CreateBook';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 class app extends Component {
+
+    // eslint-disable-next-line no-useless-constructor
+    constructor(props) {
+        super(props);
+    }
+
     render() {
         return (
-            <div className='App'>
-                <Books/>
-                <CreateBook/>
-            </div>
+            <Router>
+                <div className='App'>
+                    <Switch>
+                        <Route 
+                            path='/' exact>
+                                <Books/>
+                        </Route>
+                        <Route 
+                            path='/create' exact>
+                                <CreateBook/>
+                        </Route>
+                        <Route 
+                            path='/edit/:id' exact>
+                                <CreateBook/>
+                        </Route>
+                    </Switch>
+                </div>
+            </Router>
         )
     }
 }
--- src/index.js
+++ src/index.js
@@ -2,5 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { BrowserRouter as Router} from 'react-router-dom'
+import { createBrowserHistory } from 'history'
 
-ReactDOM.render( <App/ > , document.getElementById('root'));
+export const history = createBrowserHistory({forceRefresh:true});
+
+ReactDOM.render( 
+    <Router>
+       <App/ >  
+    </Router>, 
+    document.getElementById('root'));
